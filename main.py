f825ee49cfd3f9ad844fd5620c7e3a2f4510f481
arpan8925 <sbxp1966@gmail.com>
2024-06-23 20:56:14 +0600

    Final

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import requests
-
 NUTRITIONIX_APP_ID = "66ed0f84"
 NUTRITIONIX_API_KEY = "6f006ad8bf02725d5b7a50d2443fc172"
-MY_WEIGHT = "73"
-MY_AGE = "22"
 
-NUTRITIONIX_END = "https://trackapi.nutritionix.com/v2/natural/exercise"
-
-request_headers = {
-    "x-app-id": NUTRITIONIX_APP_ID,
-    "x-app-key": NUTRITIONIX_API_KEY,
-    "x-remote-user-id": "0"
-}
-
-requst_param = {
-    "query": "Ran 2 miles and walked for 3Km.",
-    "weight_kg": MY_WEIGHT,
-    "age": MY_AGE
-}
-
-request = requests.post(url=NUTRITIONIX_END, json=requst_param, headers=request_headers)
-
-print(request.json())
