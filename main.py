edb44dc13a2ee60c1e2152081c9cbdd576cc7887
arpan8925 <sbxp1966@gmail.com>
2024-06-24 00:48:13 +0600

    Secrets Hidden

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import datetime
+from dotenv import load_dotenv
+import os
 
-NUTRITIONIX_APP_ID = "66ed0f84"
-NUTRITIONIX_API_KEY = "6f006ad8bf02725d5b7a50d2443fc172"
+load_dotenv()
+
 MY_WEIGHT = "73"
 MY_AGE = "22"
 
@@ -14,20 +16,22 @@ date_extract = datetime.datetime.now()
 
 Excersise = input("What Excersise you did today?: ")
 
-NUTRITIONIX_END = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
 
 request_headers = {
-    "x-app-id": NUTRITIONIX_APP_ID,
-    "x-app-key": NUTRITIONIX_API_KEY,
+    "x-app-id": os.getenv("NUTRITIONIX_APP_ID"),
+    "x-app-key": os.getenv("NUTRITIONIX_API_KEY"),
     "x-remote-user-id": "0"
 }
+
 requst_param = {
     "query": Excersise,
     "age": MY_AGE,
     "weight_kg": MY_WEIGHT
 }
 
-request = requests.post(url=NUTRITIONIX_END, json=requst_param, headers=request_headers)
+request = requests.post(url=os.getenv("NUTRITIONIX_END"), json=requst_param, headers=request_headers)
+
 data = request.json()["exercises"][0]
 
 
@@ -43,11 +47,13 @@ print(f"Excersize: {excersize_name}\n\nTime: {duration_minit}\n\nCalories Burned
 
 
 
-SHEETY_POST_END = "https://api.sheety.co/7dd323f370a1fa5581a130fb15e4a412/myWorkouts/workouts"
+
+
+
 
 
 sheety_header = {
-    "Authorization": "Basic YXJwYW44OTI1OkFycGFuODkyNSQjJCM="
+    "Authorization": os.getenv("SHEETY_AUTH")
 }
 
 date = date_extract.strftime("%x")
@@ -66,6 +72,6 @@ add_row = {
 }
 
 
-add_row = requests.post(url=SHEETY_POST_END, json=add_row, headers=sheety_header)
+add_row = requests.post(url=os.getenv("SHEETY_POST_END"), json=add_row, headers=sheety_header)
 
 print(add_row.text)
